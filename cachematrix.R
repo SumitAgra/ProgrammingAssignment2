9cbf9d7653abcc0cfa65ed401aebbc6ce79a8a88
Sumit Agrawal <sumit.agra@gmail.com>
2016-09-09 16:38:12 -0400

    Uploading the Assignment 2 for week 3

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The two functions below would work together to allow user to set a matrix as input that needs to be inversed and stored in cache.
+## The second function allows user to retrieve the inverse from cache or calculate the cache and store it back in cache.
 
-## Write a short comment describing this function
+## This function would allow user to set the matrix that needs to be inversed and cached
 
 makeCacheMatrix <- function(x = matrix()) {
 
+	m <- NULL
+	set <- function(y){
+      	x<<- y
+        m<<-NULL
+	    }
+	      
+	get <- function()x
+	setinverse <- function(inv) m <<- inv
+	getinverse <- function() m
+	Tlist(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## This function retrieves the inverse of the matrix by calling first function.
+## Before running this function, it is assumed that the makeCacheMatrix has been instantiated using mkcacheobj <- makeCacheMatrix()
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+	## makecacheobj is created by instantiating makeCacheMatrix function which is how it is being referred in this function
+	m <- mkcacheobj$getinverse()
+  	if(!is.null(m)){
+      		message("getting cached data")
+          	return(m)
+	      
+	        }
+	data <- mkcacheobj$get()
+
+	## Calculating the inverse of matrix if not present in cache
+	m <- solve(data, ...)
+	
+	## Storing the inverse by calling an instance of first object
+	mkcacheobj$setinverse(m)
+
+	## returning the inverse
+	m
+
+
+	## Return a matrix that is the inverse of 'x'
+
 }
